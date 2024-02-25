4402baf1fa45720704d43a1c0e6abf1c7b48c6ac
Ano Van <anovanmaximuz@gmail.com>
2024-02-25 19:45:50 +0700

    change to micro

--- micro/apps/kitchen/src/main.ts
+++ micro/apps/kitchen/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { KitchenModule } from './kitchen.module';
-import { Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(KitchenModule);  
-  Logger.log( 'Kitchen service running on port: 3002', 'Received message:');
-  await app.listen(3002);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    KitchenModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+  await app.listen();
 }
 bootstrap();
--- micro/apps/notification/src/main.ts
+++ micro/apps/notification/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
 import { NotificationModule } from './notification.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationModule);
-  const port = 3005;  
-  Logger.log( 'Notification service running on port: '+port, 'Received message:');
-  await app.listen(port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    NotificationModule,
+    {
+      transport: Transport.TCP,
+      options: { host: '0.0.0.0', port: 5000 }
+    },
+  );
+  await app.listen();
 }
 bootstrap();
